0ae464a8ef9a34603ef7982ea678379466e60124
Karsten Müller <kmue@me.com>
2020-03-20 15:33:21 +0100

    added rm

--- script/install.sh
+++ script/install.sh
@@ -6,6 +6,7 @@ set -e
 #   sh -c "$(curl -fsSL https://raw.githubusercontent.com/karstenmueller/dotfiles/master/script/install.sh)"
 
 cd "$HOME"
+rm -rf .dotfiles
 git clone -c core.eol=lf -c core.autocrlf=false https://github.com/karstenmueller/dotfiles.git .dotfiles
 
 bash .dotfiles/script/strap.sh
